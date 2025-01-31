63c8a6b46b197a49ccd977d2074b85234fd12987
22diego <104202083+thiendoo@users.noreply.github.com>
2025-01-31 09:02:02 +0100

    Add files via upload

---
+++ Aufgabe 5/BACKUP.ps1
@@ -0,0 +1,12 @@
+﻿$SourcePath = 'C:\Users\diego\Downloads\M231'
+$BackupPath = 'B:\'
+
+$OriginalFolderName = Split-Path -Leaf $SourcePath
+
+$DestinationPath = Join-Path -Path $BackupPath -ChildPath "${OriginalFolderName}_BACKUP"
+
+New-Item -ItemType Directory -Path $DestinationPath -Force
+
+Copy-Item -Path $SourcePath -Destination $DestinationPath -Recurse -Force
+
+#Write-Host "Backup completed successfully. Files are saved to $DestinationPath"
